ee1c6b7b9485d516df75c125a6b0f18545837827
suyoung <81684148+songsuyoung@users.noreply.github.com>
2024-08-28 15:10:44 +0900

    TimeSkip 번역

--- Assets/02.Scripts/GameObject/TimeSkipUIController.cs
+++ Assets/02.Scripts/GameObject/TimeSkipUIController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TimeSkipUIController : MonoBehaviour
 {
@@ -9,15 +11,31 @@ public class TimeSkipUIController : MonoBehaviour
 
     [SerializeField]
     PlayerController playerController;
+    TranslateManager translator;
 
+    [SerializeField]
+    TMP_Text[] text;
     private void Start()
     {
         if(playerController == null)
         {
             playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
         }
-    }
+        translator = GameObject.FindWithTag("Translator").GetComponent<TranslateManager>();
 
+        translator.translatorDel += Translate;
+    }
+    public void Translate(LANGUAGE language)
+    {
+        Debug.Log("TimeSkip 번역합니다.\n");
+        int Idx = (int)language;
+
+        text[0].text = DataManager.Instance.Settings.timeSkip.title[Idx];
+
+        //아이콘
+        text[1].text = DataManager.Instance.Settings.timeSkip.yes[Idx];
+        text[2].text = DataManager.Instance.Settings.timeSkip.no[Idx];
+    }
     public void OnClick()
     {
         if (popup.activeSelf == false)
